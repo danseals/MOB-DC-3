9a1fd7fe4aafab1ce1d955ede0202a144f9031de
Dan Seals <danseals@gmail.com>
2015-07-12 20:15:50 -0400

    homework

---
+++ Homework/HW 4 - Lessons 8-10/Code/Lesson04/MapViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MapViewController.swift
+//  Lesson04
+//
+//  Created by Rudd Taylor on 9/28/14.
+//  Copyright (c) 2014 General Assembly. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    let cellID = "cellID"
+    var dict : NSMutableDictionary = ["sample": "sample"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firstTextField.delegate = self
+        secondTextField.delegate = self
+        firstTextField.tag = 100
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        
+        /*
+        TODO three: Add TWO text views and a table view to this view controller, either using code or storybaord. Accept keyboard input from the two text views. When the 'return' button is pressed on the SECOND text view, add the text view data to a dictionary. The KEY in the dictionary should be the string in the first text view, the VALUE should be the second.
+        TODO four: Make this class a UITableViewDelegate and UITableViewDataSource that supply this table view with cells that correspond to the values in the dictionary. Each cell should print out a unique pair of key/value that the map contains. When a new key/value is inserted, the table view should display it.
+        TODO five: Make the background of the text boxes in this controller BLUE when the keyboard comes up, and RED when it goes down. Start with UIKeyboardWillShowNotification and NSNotificationCenter.
+        */
+    }
+    
+    func textFieldShouldReturn(textfield: UITextField) -> Bool {
+        if textfield.tag == 100 {
+            return true
+        }
+        else {
+            self.view.endEditing(true)
+            dict["\(firstTextField.text)"] = "\(secondTextField.text)"
+            println(dict)
+            tableView.reloadData()
+            return true
+        }
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellID) as! UITableViewCell
+//        cell.textLabel?.text = array[indexPath.row]
+        
+        return cell
+    }
+}
