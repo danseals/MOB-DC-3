f8d6567ce82bf72a81cfb7b6c15a38dde1df6004
Dan Seals <danseals@gmail.com>
2015-07-20 21:15:37 -0400

    Midterm closer to complete

--- Homework/midterm/midterm/midterm/ViewController.swift
+++ Homework/midterm/midterm/midterm/ViewController.swift
@@ -9,17 +9,104 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+        var isTypingNumber = false
+        var firstNumber:Double!
+        var secondNumber:Double!
+        var operation = ""
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        firstNumber = 0
+        secondNumber = 0
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPress(sender: UIButton) {
+        
+        var number = convertToInt(sender.titleLabel!.text!)
+        
+        if isTypingNumber {
+            label.text = label.text! + "\(number)"
+        } else {
+            label.text = "\(number)"
+            isTypingNumber = true
+        }
+        
+        println("\(number)")
+        
+    }
+    
+    @IBAction func calculationPress(sender: UIButton) {
+        isTypingNumber = false
+        firstNumber = doubleFromString()
+        operation = sender.titleLabel!.text!
+        
+    }
+    
+    @IBAction func decimalPress(sender: UIButton) {
+        // Need to check if a decimal has already been added, cannot add another.
+        let currentValue = label.text!
+        let rangeOfDecimalPoint = currentValue.rangeOfString(".")
+        
+        if rangeOfDecimalPoint == nil {
+            label.text = "\(label.text!)."
+        }
+    }
+    
+    @IBAction func equalPress(sender: UIButton) {
+        isTypingNumber = false
+        var result = 0.0
+        secondNumber = doubleFromString()
+        
+        if operation == "add" {
+            result = firstNumber + secondNumber
+        } else if operation == "subtract" {
+            result = firstNumber - secondNumber
+        } else if operation == "multiply" {
+            result = firstNumber * secondNumber
+        } else if operation == "divide" {
+            result = firstNumber / secondNumber
+        }
+        
+        label.text = "\(result)"
+        
+    }
+    
+    @IBAction func invertPress(sender: UIButton) {
+        isTypingNumber = false
+        
+        if let invert = label.text?.toInt() {
+            label.text = "\(-invert)"
+        }
+    }
+    
+    @IBAction func percentPress(sender: UIButton) {
+        isTypingNumber = false
+        
+        let percent = doubleFromString()
+        label.text = "\(percent/100.0)"
+        
     }
 
+    
+    @IBAction func clearPress(sender: UIButton) {
+        firstNumber = 0
+        secondNumber = 0
+        label.text = "\(Int(firstNumber))"
+    }
+    
 
+    func convertToInt(number: String) -> Int {
+        let dict = ["zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9]
+        return dict["\(number)"]!
+    }
+    
+    func doubleFromString() -> Double {
+        let newString = label.text! as NSString
+        return newString.doubleValue
+    }
 }
 
